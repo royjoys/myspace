8c354ff5dd2a86c1a262438811ec54f79bf57d11
Soumik Roy <42010536+royjoys@users.noreply.github.com>
2019-06-12 13:42:12 +0530

    Sleuth
    Sleuth

--- movie-catalog-service/src/main/java/com/example/demo/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/example/demo/resource/MovieCatalogResource.java
@@ -3,6 +3,8 @@ package com.example.demo.resource;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +26,8 @@ public class MovieCatalogResource {
 	@Autowired
 	RestTemplate restTemplate;
 
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+
 	/*
 	 * @GetMapping("/movie") public String movieCatalog() { return "Movie"; }
 	 */
@@ -31,18 +35,19 @@ public class MovieCatalogResource {
 	@GetMapping("/{name}/{location}/movies")
 	public Theatre movieCatalogJson(@PathVariable(name = "name") String name,
 			@PathVariable(name = "location") String location) {
+		log.info("Inside movieCatalogJson with "+name +" and location "+location);
 		List<Movie> movies = movieCatalogService.getMovies(name);
 		movies = movies.stream()
-				.map(this::getRating)
-				.collect(Collectors.toList());
+						.map(this::getRating)
+						.collect(Collectors.toList());
 		Theatre t = new Theatre("1", name, location, movies);
 		return t;
 
 	}
 
 	private Movie getRating(Movie m) {
-
-		Movie forObject = restTemplate.getForObject("http://MOVIE-RATING-SERVICE/rating/" + m.getMovieId(), Movie.class);
+		Movie forObject = restTemplate.getForObject("http://MOVIE-RATING-SERVICE/rating/" + m.getMovieId(),
+				Movie.class);
 		m.setRating(forObject.getRating());
 		return m;
 	}
--- movie-rating-service/src/main/java/com/example/demo/resource/MovieRatingResource.java
+++ movie-rating-service/src/main/java/com/example/demo/resource/MovieRatingResource.java
@@ -1,5 +1,7 @@
 package com.example.demo.resource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +13,11 @@ import com.example.demo.model.Movie;
 @RequestMapping("/rating")
 public class MovieRatingResource {
 	
-	
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
 	
 	@GetMapping("/{movieId}")
 	public Movie movieCatalog(@PathVariable(name = "movieId") String movieId) {
+		log.info("Inside movieCatalog with "+movieId );
 		Movie m = new Movie();
 		m.setRating("8.5");
 		return m;
