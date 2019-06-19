eca360a91cc54b2e61a637b1c77965ab6576d5b0
Soumik Roy <42010536+royjoys@users.noreply.github.com>
2019-06-19 13:34:51 +0530

    c
    c

--- tdd-with-springboot-combined/non-reactive/src/test/java/com/example/car/web/CarsControllerTests.java
+++ tdd-with-springboot-combined/non-reactive/src/test/java/com/example/car/web/CarsControllerTests.java
@@ -18,7 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@WebMvcTest(controllers = CarsController.class)
 public class CarsControllerTests {
 
 	@Autowired
